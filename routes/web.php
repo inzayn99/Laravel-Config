57ebb6c935be32f1ad21dc7da8c5af5639e2bf16
ZAYN <60087059+inzayn99@users.noreply.github.com>
2021-03-07 16:44:10 +0545

    Laravel Initialized

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+
+Route::group(['namespace' => 'Frontend'], function () {
+    Route::any('/', 'ApplicationController@index')->name('index');
+    Route::any('/contact', 'ApplicationController@contact')->name('contact');
+
+});
